968419ad200489df992309cf71e1ebce4656efe2
EskilE <eskilme08@gmail.com>
2025-12-09 10:40:45 +0100

    removed teh js file didnt need it

--- SRC/JS/main.js
+++
@@ -1,9 +0,0 @@
-function showPopup(message) {
-  const popup = document.getElementById("popup");
-  popup.textContent = message;
-  popup.style.display = "block";
-
-  setTimeout(() => {
-    popup.style.display = "none";
-  }, 3000); // 3 seconds
-}
